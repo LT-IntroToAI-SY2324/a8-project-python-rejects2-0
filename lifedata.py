9e2811c70e9c503cbf4d1a58622da45602440c22
AR-CPS <aaramos4@cps.edu>
2024-04-29 11:09:49 -0500

    shebo

--- lifedata.py
+++ lifedata.py
@@ -1,2 +1,19 @@
+from typing import Tuple
 from neural import *
+from sklearn.model_selection import train_test_split
 
+def parse_line(line: str) -> Tuple[List[float], List[float]]:
+    """Splits line of CSV into inputs and output (transormfing output as appropriate)
+
+    Args:
+        line - one line of the CSV as a string
+
+    Returns:
+        tuple of input list and output list
+    """
+    tokens = line.split(",")
+    out = int(tokens[0])
+    output = [0 if out == 1 else 0.5 if out == 2 else 1]
+
+    inpt = [float(x) for x in tokens[1:]]
+    return (inpt, output)
