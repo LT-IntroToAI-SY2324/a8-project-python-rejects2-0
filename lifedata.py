07ba436e83659042d991876df56d2350194e0c75
C-Rosari0 <143114238+C-Rosari0@users.noreply.github.com>
2024-05-02 11:24:00 -0500

    Update lifedata.py

--- lifedata.py
+++ lifedata.py
@@ -1,15 +1,56 @@
 from typing import Tuple
 from neural import *
 from sklearn.model_selection import train_test_split
-from neural_net_UCI_data import parse_line
-from neural_net_UCI_data import normalize
 
-with open("heart_failure_clinical_records_dataset.csv", "r") as f:
-    training_data = [parse_line(line) for line in f.readlines() if len(line) > 4]
+def parse_line(line: str) -> Tuple[List[float], List[float]]:
+    """Splits line of CSV into inputs and output (transormfing output as appropriate)
 
-# print(training_data)
+    Args:
+        line - one line of the CSV as a string
+
+    Returns:
+        tuple of input list and output list
+    """
+    tokens = line.split(",")
+    out = int(tokens[0])
+    output = [0 if out == 1 else 0.5 if out == 2 else 1]
+
+    inpt = [float(x) for x in tokens[1:]]
+    return (inpt, output)
+
+
+def normalize(data: List[Tuple[List[float], List[float]]]):
+    """Makes the data range for each input feature from 0 to 1
+
+    Args:
+        data - list of (input, output) tuples
+
+    Returns:
+        normalized data where input features are mapped to 0-1 range (output already
+        mapped in parse_line)
+    """
+    leasts = len(data[0][0]) * [100.0]
+    mosts = len(data[0][0]) * [0.0]
+
+    for i in range(len(data)):
+        for j in range(len(data[i][0])):
+            if data[i][0][j] < leasts[j]:
+                leasts[j] = data[i][0][j]
+            if data[i][0][j] > mosts[j]:
+                mosts[j] = data[i][0][j]
+
+    for i in range(len(data)):
+        for j in range(len(data[i][0])):
+            data[i][0][j] = (data[i][0][j] - leasts[j]) / (mosts[j] - leasts[j])
+    return data
+
+
+with open("heart_failure_clinical_records_dataset.csv", "r") as raw_data:
+    training_data = [parse_line(line) for line in raw_data.readlines()]
+
+print(training_data)
 td = normalize(training_data)
-# print(td)
+print(td)
 
 train, test = train_test_split(td)
 
